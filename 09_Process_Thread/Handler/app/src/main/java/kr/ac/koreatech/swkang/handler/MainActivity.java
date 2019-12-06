2dce075016254d7ef5159f4c10a13f238d3354fb
김승규 <rlatmdrb1996@koreatech.ac.kr>
2019-12-06 15:49:34 +0900

    final

--- 09_Process_Thread/Handler/app/src/main/java/kr/ac/koreatech/swkang/handler/MainActivity.java
+++ 09_Process_Thread/Handler/app/src/main/java/kr/ac/koreatech/swkang/handler/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.ac.koreatech.swkang.handler;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
 
     // Handler 객체 생성
-    // Handler mHandler =
+    Handler mHandler = new Handler();
 
     // 작업 스레드로 사용할 Thread 클래스의 subclass 정의
     class CountThread extends Thread {
@@ -22,10 +23,15 @@ public class MainActivity extends AppCompatActivity {
             int i = 0;
             for(i=0; i < 20 && running; i++) {
                 // count 값 1 증가
-
+                mCount++;
                 // UI 갱신 작업 (TextView의 count 수 변경)을 Runnable 인터페이스의
                 // run() 함수 내에 구현하고 Handler 객체의 post 함수로 넘겨준다
-
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText("Count: " + mCount);
+                    }
+                });
 
                 try {
                     // thread 1초간 sleep
@@ -50,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         // 작업 스레드 객체 생성
-
+        w = new CountThread();
+        w.start();
 
         // 작업 스레드 시작
 
@@ -67,6 +74,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View vieW) {
         // TextView의 count 값 갱신
-
+        tv.setText("mCount: " + mCount);
     }
 }
